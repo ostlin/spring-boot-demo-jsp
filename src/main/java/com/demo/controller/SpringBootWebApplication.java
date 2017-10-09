74ad520c25ec456797174b5f06849d0c3036a01c
Magnus Östlin <magnus.ostlin@lm.se>
2017-10-09 12:58:31 +0200

    Initial commit

---
+++ src/main/java/com/demo/controller/SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.demo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+}
